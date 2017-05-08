043ce56765532319bba057828106b02cc4a69fd2
a1703500 <a1703500@ccf68bb9-d9f7-4bd8-892d-a681e0b0e2f6>
2017-05-08 04:18:04 +0000

    adding quick sort function

--- Sort.cpp
+++ Sort.cpp
@@ -24,12 +24,16 @@ std::vector<int> BubbleSort::sortFunc( std::vector<int> vec, int a, int b )
 	int sec = b;
 	int size = vec.size();
 
+	// Checks to make sure the iterator does not exceed the length of the vector given.
 	for ( int i = 0; i < size; i++ )
 	{
+		// Loops to the length of the vector, minus one.
 		for ( int j = 0; j < size - 1; j++ )
 		{
+			// If int at position j is greater than the integer at position j+1 is larger...
 			if ( vec.at(j) > vec.at(j+1) )
 			{
+				// Swap integer at position j to position of j+1 and vice-versa.
 				int temp = vec.at(j+1);
 				vec.at(j+1) = vec.at(j);
 				vec.at(j) = temp;
@@ -42,6 +46,57 @@ std::vector<int> BubbleSort::sortFunc( std::vector<int> vec, int a, int b )
 
 // BubbleSort class destructor.
 BubbleSort::~BubbleSort()
+{
+
+}
+
+// QuickSort class constructor.
+QuickSort::QuickSort()
+{
+
+}
+
+// Find and set the pivot position to be used in the QuickSort function 'sortFunc'.
+// Takes in a vector, the first integer of that vector and the last integer of that vector.
+std::vector<int> QuickSort::sortFunc(std::vector<int> vec, int left, int right) {
+    int i = left, j = right;
+    int tmp;
+    int pivot;
+    
+    if ( vec.size() >= 3 )
+    {
+    	pivot = vec.at(2);
+    }
+    else
+    {
+    	pivot = vec.at( (left + right) / 2 );
+
+ 	}
+    /* partition */
+    std::cout << "pivot = " << pivot << "\n";
+    while (i <= j) { 
+        while (vec.at(i) < pivot)
+            i++;
+        while (vec.at(j) > pivot)
+            j--;
+        if (i <= j) {
+            tmp = vec.at(i);
+            vec.at(i) = vec.at(j);
+            vec.at(j) = tmp;
+            i++;
+            j--;
+        }
+    }
+
+    /* recursion */
+    if (left < j)
+        sortFunc( vec, left, j );
+    if (i < right)
+        sortFunc( vec, i, right );
+}
+
+// QuickSort class destructor.
+QuickSort::~QuickSort()
 {
 
 }
--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ int main()
 
     // The user inputs a string of numbers (e.g. "6 4 -2 88 ..etc") and those integers are then put into a vector named 'vec'.
 	std::vector<int> vec;
+	int vecSize = vec.size();
 
 	std::string line;
    	if ( getline(std::cin, line) )
@@ -29,13 +30,38 @@ int main()
    	// Creating BubbleSort object.
    	BubbleSort bSort;
    	BubbleSort *ptrBSort = &bSort;
+   	// Creating new vector that has been 'Bubble Sorted'.
+   	std::vector<int> bSortedVec;
+   	bSortedVec = ptrBSort->sortFunc( vec, vec.at(0), vec.at(1) );
+
+   	// Creating QuickSort object
+   	QuickSort qSort;
+   	QuickSort *ptrQSort = &qSort;
+   	// Creating new vector that has been 'Quick Sorted'.
+   	std::vector<int> qSortedVec;
+   	qSortedVec = ptrQSort->sortFunc( vec, 0, vec.size()-1 );
 
 /*--------------------------------------OUTPUT-----------------------------------------------------------------------*/
 
    	// Print out inputted integers.
+   	std:: cout << "Original list of integers: \n";
    	for ( int i = 0; i < vec.size(); i++ )
    	{
    		std::cout << vec[i] << " ";
+   	}
+   	std::cout << "\n";
+
+   	std::cout << "Bubble Sorted integers: \n";
+   	for ( int i = 0; i < bSortedVec.size(); i++ )
+   	{
+   		std::cout << bSortedVec[i] << " ";
+   	}
+   	std::cout << "\n";
+
+   	std::cout << "Quick Sorted integers: \n";
+   	for ( int i = 0; i < qSortedVec.size(); i++ )
+   	{
+   		std::cout << qSortedVec[i] << " ";
    	}
    	std::cout << "\n";
 

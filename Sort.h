576af2b88104290a85025c1e0e37aad31259ba60
a1703500 <a1703500@ccf68bb9-d9f7-4bd8-892d-a681e0b0e2f6>
2017-05-08 00:57:04 +0000

    creating bubbleSort

--- Sort.h
+++ Sort.h
@@ -1,36 +1,37 @@
 #include <iostream>
 #include <vector>
 
+// Base class for sorting function.
 class Sort
 {
-private:
-	int a;
-	int b;
-	std::vector<int> vec;
-	virtual int sortFunc( std::vector<int> vec, int a, int b );
+	protected:
+		//std::vector<int> vec;
+		virtual std::vector<int> sortFunc( std::vector<int> vec, int a, int b ) = 0;
 
-public:
-	Sort();
-	~Sort();
+	public:
+		~Sort();
 };
 
+// Class for creating Bubble Sort algorithm, a child class of base class 'Sort'.
 class BubbleSort : public Sort
 {
-private:
-	int sortFunc( std::std::vector<int> vec, int a, int b );
+	protected:
+		std::vector<int> sortFunc( std::vector<int> vec, int a, int b );
 
-public:
-	BubbleSort();
-	~BubbleSort();
+	public:
+		BubbleSort();
+		~BubbleSort();
 };
-
+/*
+// Class for creating Quick Sort algorithm, a child class of the base class 'Sort'.
 class QuickSort : public Sort
 {
-private:
-	int pivot;
-	int sortFunc( std::std::vector<int> vec, int a, int b );
+	private:
+		int pivot;
+		std::vector<int> sortFunc( std::std::vector<int> vec, int a, int b );
 
-public:
-	QuickSort();
-	~QuickSort();
+	public:
+		QuickSort();
+		~QuickSort();
 };
+*/

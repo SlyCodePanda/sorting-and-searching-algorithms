222dc5ec7d5231b7afdb901ffacc865b6a7c5a51
a1703500 <a1703500@ccf68bb9-d9f7-4bd8-892d-a681e0b0e2f6>
2017-05-08 10:43:44 +0000

    functional search function

--- 2017/s1/adds/assignment8/RecursiveBinarySearch.h
+++ 2017/s1/adds/assignment8/RecursiveBinarySearch.h
@@ -4,9 +4,10 @@
 class RecursiveBinarySearch
 {
 public:
-	int obj;
+	// The searched for value for this program will always be '1'. Which we will be referred to as 'obj' in the binarySearch function.
+	int obj = 1;
 
-	bool binarySearch( std::vector<int> arr, int mid );
+	bool binarySearch( std::vector<int> arr, int mid, int start, int end );
 
 	RecursiveBinarySearch();
 	~RecursiveBinarySearch();

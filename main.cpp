602a35c4a5cf14c8a49007a0578fef4946518341
a1703500 <a1703500@ccf68bb9-d9f7-4bd8-892d-a681e0b0e2f6>
2017-05-08 00:00:24 +0000

    adding user input

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
+#include <string>
 
-#include "Sort.h"
-#include "RecursiveBinarySearch.h"
+//#include "Sort.h"
+//#include "RecursiveBinarySearch.h"
 
 int main()
 {
+
+    // The user inputs a string of numbers (e.g. "6 4 -2 88 ..etc") and those integers are then put into a vector named 'vec'.
+	std::vector<int> vec;
+
+	std::string line;
+   	if ( getline(std::cin, line) )
+   	{
+    	std::istringstream str(line);
+
+      	int value;
+      	str >> value;
+      	vec.push_back( value );
+      	while ( str >> value ) 
+      	{
+         	vec.push_back( value );
+      	}
+   	}
+
+/*--------------------------------------OUTPUT-----------------------------------------------------------------------*/
+   	
+   	// Print out inputted integers.
+   	for ( int i = 0; i < vec.size(); i++ )
+   	{
+   		std::cout << vec[i] << " ";
+   	}
+   	std::cout << "\n";
 
 	return 0;
 }

45314efe0e153a2eda633c14989e9297ef63ce83
a1703500 <a1703500@ccf68bb9-d9f7-4bd8-892d-a681e0b0e2f6>
2017-05-09 09:52:51 +0000

    adjustments to main

--- main.cpp
+++ main.cpp
@@ -27,60 +27,46 @@ int main()
       	}
    	}
 
-   	BubbleSort bSort;
-   	BubbleSort *ptrBSort = &bSort;
-   	std::vector<int> bSortedVec;
-   	int vecSize2 = vec.size();
-   	bSortedVec = ptrBSort->sortFunc( vec, vec.at(0), vec.at(vecSize2-1) );
+  // Creating QuickSort object.
+  QuickSort qSort;
+  QuickSort *ptrQSort = &qSort;
 
-   	// Creating QuickSort object.
-   	QuickSort qSort;
-   	QuickSort *ptrQSort = &qSort;
-   	// Creating new vector that has been 'Quick Sorted'.
-   	int vecSize = vec.size();
-   	std::vector<int> qSortedVec;
-   	qSortedVec = ptrQSort->sortFunc( vec, 0, vecSize-1 );
+  // Creating new vector that has been 'Quick Sorted'.
+  int vecSize = vec.size();
+  std::vector<int> qSortedVec;
+  qSortedVec = ptrQSort->sortFunc( vec, 0, vecSize-1 );
 
-   	// Middle, start, and end positions on the vector.
-   	int mid = ( 0 + (vec.size()-1) )  / 2;
-   	int start = 0, end = vec.size() - 1;
+  // Middle, start, and end positions on the vector.
+  int mid = ( 0 + (vec.size()-1) )  / 2;
+  int start = 0, end = vec.size() - 1;
 
-    // Creating RecursiveBinarySearch object.
-    RecursiveBinarySearch bSearch;
-    RecursiveBinarySearch *ptrBSearch = &bSearch;
-    //bool bS = ptrBSearch->binarySearch( qSortedVec, mid, start, end );
-    bool bS = ptrBSearch->binarySearch( bSortedVec, mid, start, end );
-
+  // Creating RecursiveBinarySearch object.
+  RecursiveBinarySearch bSearch;
+  RecursiveBinarySearch *ptrBSearch = &bSearch;
+  bool bS = ptrBSearch->binarySearch( qSortedVec, mid, start, end );
 
 /*--------------------------------------OUTPUT-----------------------------------------------------------------------*/
 
-   	// Print out inputted integers and the binary search result.
+  // Print out inputted integers and the binary search result.
 
-    // Depending on the binary search, print either 'true' or 'false'.
-    if ( bS == 1 )
-    {
-    	std::cout << "true ";
-    }
-    if ( bS == 0 )
-    {
-    	std::cout << "false ";
-    }
-/*
-    // Prints the result of the 'quick sorted' array.
-    int sortedSize = qSortedVec.size();
-   	for ( int i = 0; i < sortedSize; i++ )
-   	{
-   		std::cout << qSortedVec[i] << " ";
-   	}
-   	std::cout << "\n";
-*/
-    // Prints the result of the 'quick sorted' array.
-    int sortedSize = bSortedVec.size();
-   	for ( int i = 0; i < sortedSize; i++ )
-   	{
-   		std::cout << bSortedVec[i] << " ";
-   	}
-   	std::cout << "\n";
+  // Depending on the binary search, print either 'true' or 'false'.
+  if ( bS == 1 )
+  {
+  	std::cout << "true ";
+  }
+  if ( bS == 0 )
+  {
+  	std::cout << "false ";
+  }
+
+  // Prints the result of the 'quick sorted' array.
+  int sortedSize = qSortedVec.size();
+  for ( int i = 0; i < sortedSize; i++ )
+  {
+  	std::cout << qSortedVec[i] << " ";
+  }
+  std::cout << "\n";
 
 	return 0;
+
 }
